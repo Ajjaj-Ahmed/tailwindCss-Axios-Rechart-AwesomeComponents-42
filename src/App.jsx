c4b1ae799a252a11fc57288ab40dda4f0396d08c
Ajjaj-Ahmed <ajjajahmedasif@gmail.com>
2024-03-19 12:11:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl bg-green-200 p-5'>React</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
